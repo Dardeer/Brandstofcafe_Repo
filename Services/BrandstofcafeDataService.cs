05111139cb72e08a5816931af6d7f17fd7689eb9
Dian's GitHub <diandard16@gmail.com>
2025-06-08 22:44:39 +0200

    FEAT: Implementatie skelet van BrandstofcafeDataService met placeholder methoden

---
+++ Services/BrandstofcafeDataService.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using MySqlConnector;
+using NHL_Brandstofcafe.Models;
+using NHL_Brandstofcafe.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NHL_Brandstofcafe.Services
+{
+    public class BrandstofcafeDataService : IBrandstofcafeDataService
+    {
+        private readonly string _connectionString;
+
+        public BrandstofcafeDataService(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<List<Sectie>> GetSectiesAsync()
+        {
+            await Task.Delay(1);
+            return new List<Sectie>();
+        }
+
+        public async Task<List<Tafel>> GetTafelsBySectieIdAsync(int sectieId)
+        {
+            await Task.Delay(1);
+            return new List<Tafel>();
+        }
+    }
+}
