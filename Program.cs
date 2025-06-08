f71d3cdcf471ad070307ecfde137155c03b41aa6
Dian's GitHub <diandard16@gmail.com>
2025-06-08 23:15:32 +0200

    FEAT: configureer Dependency Injection in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
 using NHL_Brandstofcafe.Components;
+using NHL_Brandstofcafe.Services; // IBrandstofcafeDataService en BrandstofcafeDataService toevoegen
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+// --- START NIEUWE CODE --- 
+// De connection string uit appsettings.json ophalen
+var connectionString = builder.Configuration.GetConnectionString("BrandstofcafeDb")
+    ?? throw new InvalidOperationException
+    ("Verbindingsreeks 'BrandstofcafeDb' niet gevonden in appsettings.json, BrandstofcafeDataService kan niet worden geregistreerd!");
+
+// Registreer IBrandstofcafeDataService en BrandstofcafeDataService voor Dependency injection
+builder.Services.AddScoped<IBrandstofcafeDataService>(provider => new BrandstofcafeDataService(connectionString));
+// --- EIND NIEUWE CODE --- 
 
 var app = builder.Build();
 
